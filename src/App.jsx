a80203372e322de1cc98f59af79917fbf02b2d51
Sarav <Saravjeetofficial@gmail.com>
2025-06-18 17:56:25 +0530

    added the assets folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-function App() {
- 
+import React from 'react'
 
+const App = () => {
   return (
-    <>
-     <h1 className="text-5xl text-red-400">Hello world</h1>
-    </>
+    <div>
+      
+    </div>
   )
 }
 
