a36dcdc0a95f9321d0f9e2120557e663773ded86
Sarav <Saravjeetofficial@gmail.com>
2025-06-23 21:26:15 +0530

    fixed text

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -15,7 +15,7 @@ const Navbar = () => {
 
   return (
     <nav className="py-3 sticky top-0 z-50 backdrop-blur-lg border-b border-neutral-700/80">
-      <div className="container px-4 mx-auto relative text-base">
+      <div className="container px-4 mx-auto relative lg:text-sm">
         <div className="flex justify-between items-center">
           <div className="flex items-center flex-shrink-0">
             <img className='w-10 h-10 mr-2' src={logo} alt="" />
