0d3da53f9bae815f353945e848ac52d919e597a6
Sarav <Saravjeetofficial@gmail.com>
2025-07-16 14:50:32 +0530

    committing all changes

--- frontend/src/components/Signup.jsx
+++
@@ -1,9 +0,0 @@
-const Signup = () => {
-  return (
-    <div className="min-h-screen flex items-center justify-center text-3xl font-bold">
-      Signup Page
-    </div>
-  );
-};
-
-export default Signup;
