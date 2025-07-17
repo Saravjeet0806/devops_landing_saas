dc77e1e9546a73f35359dd5ce4de92be8ffa08f4
Sarav <Saravjeetofficial@gmail.com>
2025-07-17 22:13:05 +0530

    added new video

--- frontend/src/components/HeroSection.jsx
+++ frontend/src/components/HeroSection.jsx
@@ -21,7 +21,7 @@ const HeroSection = () => {
           Your browser does not support the video tag.
         </video>
         <video
-          autoPlay loop muted className="rounded-lg w-1/2 border border-orange-700 shadow-sm shadow-orange-400 mx-2 my-4">
+          autoPlay loop muted className="rounded-lg w-1/2 border border-orange-700 shadow-sm shadow-orange-400 mx-2 my-4 ">
           <source src={video2} type="video/mp4" />
           Your browser does not support the video tag.
         </video>
